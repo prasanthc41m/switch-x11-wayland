83415322eaf45b5c20105dca9ebe327f7b087bdf
Prasanth K <58906808+prasanthc41m@users.noreply.github.com>
2024-12-25 12:57:54 +0530

    Add files via upload

--- extension.js
+++ extension.js
@@ -1,89 +1,105 @@
-const {St, Clutter, GObject, GLib} = imports.gi;
-const Main = imports.ui.main;
-const PanelMenu = imports.ui.panelMenu;
-const PopupMenu = imports.ui.popupMenu;
-const Util = imports.misc.util;
-const ExtensionUtils = imports.misc.extensionUtils;
-const Gio = imports.gi.Gio;
+import St from 'gi://St';
+import Clutter from 'gi://Clutter';
+import GObject from 'gi://GObject';
+import GLib from 'gi://GLib';
+import Gio from 'gi://Gio';
+import * as Main from 'resource:///org/gnome/shell/ui/main.js';
+import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js';
+import * as PopupMenu from 'resource:///org/gnome/shell/ui/popupMenu.js';
+import * as Util from 'resource:///org/gnome/shell/misc/util.js';
 
 let _indicator;
 
+const HomePath = GLib.get_home_dir();
+const ExtensionPath = `${HomePath}/.local/share/gnome-shell/extensions/switch-x11-wayland@prasanthc41m.github.com/`;
+
 const SessionIndicator = GObject.registerClass(
-    {GTypeName: 'SessionIndicator'},
     class SessionIndicator extends PanelMenu.Button {
         _init() {
             super._init(0.0, 'Session Indicator');
 
             let box = new St.BoxLayout();
-            this._icon = new St.Icon({style_class: 'session-indicator-icon'});
-            box.add(this._icon);
+            this._icon = new St.Icon({
+                style_class: 'system-status-icon',
+            });
+            box.add_child(this._icon);
             this.add_child(box);
 
-            this.x11MenuItem = new PopupMenu.PopupSwitchMenuItem('X11', false);
-            this.waylandMenuItem = new PopupMenu.PopupSwitchMenuItem('Wayland', false);
+            this.initializing = true;
+
+            this.x11MenuItem = new PopupMenu.PopupSwitchMenuItem('Switch to X11', false);
+            this.waylandMenuItem = new PopupMenu.PopupSwitchMenuItem('Switch to Wayland', false);
 
             this.x11MenuItem.connect('toggled', () => {
-                if (this.x11MenuItem.state) {
+                if (this.x11MenuItem.state && !this.initializing) {
                     this.waylandMenuItem.setToggleState(false);
-                    const Me = ExtensionUtils.getCurrentExtension();
-                    Util.spawn(['pkexec', 'bash', Me.path + '/x11.sh']);
+                    Util.spawn(['pkexec', 'bash', `${ExtensionPath}x11.sh`]);
                     GLib.setenv('GNOME_SESSION_TYPE', 'x11', true);
                     Main.notify("Session change", "Please reboot to switch default session to X11.");
+                    this._updateIconAndMenu('x11');
                 }
             });
 
             this.waylandMenuItem.connect('toggled', () => {
-                if (this.waylandMenuItem.state) {
+                if (this.waylandMenuItem.state && !this.initializing) {
                     this.x11MenuItem.setToggleState(false);
-                    const Me = ExtensionUtils.getCurrentExtension();
-                    Util.spawn(['pkexec', 'bash', Me.path + '/wayland.sh']);
+                    Util.spawn(['pkexec', 'bash', `${ExtensionPath}wayland.sh`]);
                     GLib.setenv('GNOME_SESSION_TYPE', 'wayland', true);
                     Main.notify("Session change", "Please reboot to switch default session to Wayland.");
+                    this._updateIconAndMenu('wayland');
                 }
             });
 
             this.menu.addMenuItem(this.x11MenuItem);
             this.menu.addMenuItem(this.waylandMenuItem);
 
-            let sessionType = GLib.getenv('GNOME_SESSION_TYPE');
-            if (sessionType === 'x11') {
-                this.x11MenuItem.setToggleState(true);
-                this.waylandMenuItem.setToggleState(false);
-            } else if (sessionType === 'wayland') {
-                this.x11MenuItem.setToggleState(false);
-                this.waylandMenuItem.setToggleState(true);
-            }
-
-            this._updateIcon();
+            // Set the initial state based on the current windowing system, but do not execute any change commands
+            this._setInitialState();
+            this.initializing = false;
         }
 
-        _updateIcon() {
-            const Me = ExtensionUtils.getCurrentExtension();
-            let iconName;
+        _setInitialState() {
             let sessionType = GLib.getenv('XDG_SESSION_TYPE');
+            this._updateIcon(sessionType);
             if (sessionType === 'x11') {
-                iconName = Me.path + '/icons/x11.svg';
-                this._icon.gicon = Gio.icon_new_for_string(iconName);
                 this.x11MenuItem.setToggleState(true);
                 this.waylandMenuItem.setToggleState(false);
             } else if (sessionType === 'wayland') {
-                iconName = Me.path + '/icons/wayland.svg';
-                this._icon.gicon = Gio.icon_new_for_string(iconName);
                 this.x11MenuItem.setToggleState(false);
                 this.waylandMenuItem.setToggleState(true);
             }
+        }
+
+        _updateIcon(sessionType) {
+            let iconName;
+            if (sessionType === 'x11') {
+                iconName = `${ExtensionPath}icons/x11.svg`;
+            } else if (sessionType === 'wayland') {
+                iconName = `${ExtensionPath}icons/wayland.svg`;
+            }
+            this._icon.set_gicon(Gio.icon_new_for_string(iconName));
+        }
+
+        _updateIconAndMenu(type = null) {
+            let sessionType = type || GLib.getenv('XDG_SESSION_TYPE');
+            this._updateIcon(sessionType);
         }
     }
 );
 
-function init() {}
+let indicator;
 
-function enable() {
-    _indicator = new SessionIndicator();
-    Main.panel.addToStatusArea('session-indicator', _indicator);
+export default class X11Extension {
+    enable() {
+        indicator = new SessionIndicator();
+        Main.panel.addToStatusArea('session-indicator-switch', indicator); // Unique role name
+    }
+
+    disable() {
+        if (indicator) {
+            indicator.destroy();
+            indicator = null;
+        }
+    }
 }
 
-function disable() {
-    _indicator.destroy();
-    _indicator = null;
-}
